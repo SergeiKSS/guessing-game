48a918d70c2b8b999eb35b4badd81bc199483cb2
SergeiKSS <SergeiKSS@mail.ru>
2017-02-04 15:28:21 +0200

    guessing-game:implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+		var min = 0, max = 0, x=0;
+		this.min = 0;
+    	this.max = 0;
+    	this.x = 0;
+	}
 
     setRange(min, max) {
-
+		this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+		this.x = Math.ceil((this.min + this.max) / 2);
+		return this.x;
     }
 
     lower() {
-
+		this.setRange(this.min, this.x);
     }
 
     greater() {
-
+		this.setRange(this.x, this.max);
     }
 }
 
